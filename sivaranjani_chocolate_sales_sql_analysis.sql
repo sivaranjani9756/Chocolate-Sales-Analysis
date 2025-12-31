75cd25f2f1e5f5f906651c806908cd4b68b06736
sivaranjani koduru <sivaranjani9756@gmail.com>
2025-12-31 17:05:07 +0530

    Add files via upload

---
+++ sivaranjani_chocolate_sales_sql_analysis.sql
@@ -0,0 +1,7 @@
+use sivaranjani_chocolate_sales_db;
+select count(*) from shipments;
+select count(*) from calendar;
+select count(*) from locations;
+select count(*) from people ;
+select count(*) from products;
+
